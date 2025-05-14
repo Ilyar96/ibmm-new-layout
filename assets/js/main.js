f267ecb7501f19129125ff55d50c84fa7e4fb964
Ilyar Kassymov <kasyymov.ilyar@gmail.com>
2025-05-14 12:51:46 +0300

    fix slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,30 @@ function initSliders() {
 		scrollbar: {
 			el: ".recommends__slider-scrollbar",
 		},
+		spaceBetween: 16,
+		breakpoints: {
+			1200: {
+				spaceBetween: 0,
+			},
+			992: {
+				spaceBetween: 29,
+			},
+		},
 	});
 
 	new Swiper("#shipment-slider", {
 		slidesPerView: "auto",
 		scrollbar: {
 			el: ".channel__section-head-scrollbar",
+		},
+		spaceBetween: 16,
+		breakpoints: {
+			1200: {
+				spaceBetween: 0,
+			},
+			992: {
+				spaceBetween: 29,
+			},
 		},
 	});
 
@@ -29,6 +47,15 @@ function initSliders() {
 		navigation: {
 			nextEl: "#top-miners-next",
 			prevEl: "#top-miners-prev",
+		},
+		spaceBetween: 16,
+		breakpoints: {
+			1200: {
+				spaceBetween: 0,
+			},
+			992: {
+				spaceBetween: 29,
+			},
 		},
 	});
 }
