3c188387c0100d7d8d3216b0a8d604e4714988a3
Ilyar Kassymov <kasyymov.ilyar@gmail.com>
2025-06-29 16:59:02 +0300

    form? refactor

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,12 +1,18 @@
 (function () {
-	const prevBtnSelector = "#product-gallery-1-prev";
-	const nextBtnSelector = "#product-gallery-1-next";
+	initSliders();
 
-	new Swiper("#product-gallery-1", {
-		slidesPerView: "auto",
-		navigation: {
-			nextEl: nextBtnSelector,
-			prevEl: prevBtnSelector,
-		},
-	});
+	// Sliders
+	function initSliders() {
+		// Product gallery 1
+		const prevBtnSelector = "#product-gallery-1-prev";
+		const nextBtnSelector = "#product-gallery-1-next";
+
+		new Swiper("#product-gallery-1", {
+			slidesPerView: "auto",
+			navigation: {
+				nextEl: nextBtnSelector,
+				prevEl: prevBtnSelector,
+			},
+		});
+	}
 })();
